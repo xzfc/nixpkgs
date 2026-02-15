6d6be1a55725240a1b45bed6e3b93035b1d12b45
xzfc <xzfcpw@gmail.com>
2026-02-15 00:45:52 +0000

    intel-oneapi-vtune: init at 2025.8

---
+++ pkgs/by-name/in/intel-oneapi-vtune/package.nix
@@ -0,0 +1,144 @@
+{
+  alsa-lib,
+  atk,
+  autoPatchelfHook,
+  cairo,
+  cups,
+  dbus,
+  elfutils,
+  expat,
+  fetchurl,
+  file-rename,
+  glib,
+  gtk3,
+  kmod,
+  lib,
+  libx11,
+  libxcomposite,
+  libxdamage,
+  libxext,
+  libxfixes,
+  libxrandr,
+  libdrm,
+  libndctl,
+  libsafec,
+  libxcb,
+  libxcrypt-legacy,
+  libxkbcommon,
+  mesa,
+  ncurses5,
+  nspr,
+  nss,
+  p7zip,
+  pango,
+  stdenv,
+  systemd,
+  wrapGAppsHook3,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "intel-oneapi-vtune";
+  version = "2025.8";
+
+  src = fetchurl {
+    # To get this URL, download an Online or Offline Installer[1], extract it,
+    # and find the URL in the corresponding `manifest.json` file.
+    # [1]: https://www.intel.com/content/www/us/en/developer/tools/oneapi/vtune-profiler-download.html
+    url = "https://installer.repos.intel.com/oneapi/vtune/lin/intel.oneapi.lin.vtune,v=${finalAttrs.version}.1%2B5/cupPayload.cup";
+    hash = "sha256-wRZvR+gkJPHrJccbvdXBRocrjCi80oVAzkPe1C3ciyQ=";
+  };
+
+  nativeBuildInputs = [
+    autoPatchelfHook
+    file-rename
+    p7zip
+    wrapGAppsHook3
+  ];
+
+  buildInputs = [
+    alsa-lib
+    atk
+    cairo
+    cups
+    dbus
+    elfutils
+    expat
+    glib
+    gtk3
+    kmod
+    libx11
+    libxcomposite
+    libxdamage
+    libxext
+    libxfixes
+    libxrandr
+    libdrm
+    libndctl
+    libsafec
+    libxcb
+    libxcrypt-legacy
+    libxkbcommon
+    mesa
+    ncurses5
+    nspr
+    nss
+    pango
+    stdenv.cc.cc.lib
+  ];
+
+  unpackPhase = ''
+    runHook preUnpack
+
+    7za x $src _installdir/vtune/${finalAttrs.version}
+
+    # Fix percent-encoded filenames, e.g. "libstdc%2B%2B.so.6" -> "libstdc++.so.6"
+    find -depth -name '*%*' -execdir rename 's/%2B/+/g; s/%5B/[/g; s/%5D/]/g' {} \;
+
+    runHook postUnpack
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/opt/intel/oneapi
+    mv _installdir/vtune $out/opt/intel/oneapi
+    ln -s $out/opt/intel/oneapi/vtune/{${finalAttrs.version},latest}
+
+    mkdir -p $out/bin
+    for bin in vtune vtune-backend vtune-gui; do
+      ln -s $out/opt/intel/oneapi/vtune/${finalAttrs.version}/bin64/$bin $out/bin/
+    done
+
+    mkdir -p $out/share/applications
+    cp $out/opt/intel/oneapi/vtune/${finalAttrs.version}/bin64/vtune-gui.desktop $out/share/applications/
+    sed -i $out/share/applications/vtune-gui.desktop -e "
+      s|^Exec=.*|Exec=vtune-gui|g;
+      s|^Icon=./|Icon=$out/opt/intel/oneapi/vtune/${finalAttrs.version}/bin64/|g;
+    "
+
+    runHook postInstall
+  '';
+
+  autoPatchelfIgnoreMissingDeps = [
+    "libffi.so.6" # Wanted by vendored python
+    "libgdbm.so.4" # Wanted by vendored python
+    "libgdbm_compat.so.4" # Wanted by vendored python
+    "libreadline.so.6" # Wanted by vendored python
+    "libopencl-clang.so.14" # Wanted by bin64/gma/GTPin/Profilers/ocloc/Bin/intel64/libigdfcl.so
+    "libsycl.so.8" # Wanted by bin64/self_check_apps/matrix.dpcpp/matrix.dpcpp
+  ];
+
+  runtimeDependencies = [
+    systemd # for zygote (vtune-gui)
+  ];
+
+  meta = {
+    description = "Performance analysis tool for x86-based machines";
+    homepage = "https://www.intel.com/content/www/us/en/developer/tools/oneapi/vtune-profiler.html";
+    license = lib.licenses.unfree;
+    maintainers = [
+      lib.maintainers.xzfc
+    ];
+    platforms = [ "x86_64-linux" ];
+  };
+})
